52c00e921ed24d7f3886a4113ab4103bfe4caa58
JunHyeok Heo <junhyeokheo@JunHyeokui-MacBookPro.local>
2020-03-29 20:08:29 +0800

    Updates

---
+++ precache-manifest.f5e8f595e4d53b4963d68898fe40b308.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df2720ef8fec2c6aa57132a85be14339",
+    "url": "superZUN.github.io/etymomap/index.html"
+  },
+  {
+    "revision": "ec895a2431c7faeadfb4",
+    "url": "superZUN.github.io/etymomap/static/css/main.5a9b470d.chunk.css"
+  },
+  {
+    "revision": "828a530485c225743491",
+    "url": "superZUN.github.io/etymomap/static/js/2.cd9a7021.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "superZUN.github.io/etymomap/static/js/2.cd9a7021.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec895a2431c7faeadfb4",
+    "url": "superZUN.github.io/etymomap/static/js/main.1406a404.chunk.js"
+  },
+  {
+    "revision": "64c4957c20dee5453b46",
+    "url": "superZUN.github.io/etymomap/static/js/runtime-main.0f3ee85f.js"
+  }
+]);
