9dd175cbe4fd45ff69168aacdb39c600f69a67b4
JunHyeok Heo <junhyeokheo@JunHyeokui-MacBookPro.local>
2020-04-01 23:22:46 +0800

    Updates

---
+++ precache-manifest.fe2533ae6347a954868f1ea7d7cb3d5f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19f386f6396c8abbcff193c03f796e8e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9a6519915bc92366128a",
+    "url": "/static/css/main.5a9b470d.chunk.css"
+  },
+  {
+    "revision": "828a530485c225743491",
+    "url": "/static/js/2.cd9a7021.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.cd9a7021.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a6519915bc92366128a",
+    "url": "/static/js/main.89fe70f2.chunk.js"
+  },
+  {
+    "revision": "550bdda565857905d20d",
+    "url": "/static/js/runtime-main.05809df7.js"
+  }
+]);
